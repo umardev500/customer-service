23c811327903905debe9ecf04778197199d7cbfe
umar <umar.dev500@gmail.com>
2023-02-17 06:46:13 +0700

    update not empty filter

--- helper/no_empty.go
+++ helper/no_empty.go
@@ -4,23 +4,15 @@ import (
 	"reflect"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func NoEmpty(source bson.D, dest *bson.D) {
 	for _, val := range source {
-		passToField := func() {
-			detailValue := bson.E{Key: val.Key, Value: val.Value}
-			*dest = append(*dest, detailValue)
-		}
+		isZero := reflect.ValueOf(val.Value).IsZero()
 
-		isMap := reflect.TypeOf(val.Value) == reflect.TypeOf(primitive.M{})
-		if isMap {
-			if len(val.Value.(primitive.M)) > 0 {
-				passToField()
-			}
-		} else if len(val.Value.(string)) > 0 {
-			passToField()
+		if !isZero {
+			each := bson.E{Key: val.Key, Value: val.Value}
+			*dest = append(*dest, each)
 		}
 	}
 }
